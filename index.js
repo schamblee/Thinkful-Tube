fee9a7fa7a882be80f3d4c95b90ee9109f78591f
Stephanie Chamblee <stephaniejoychamblee@gmail.com>
2018-02-10 09:57:10 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,38 @@
+const YOUTUBE_SEARCH_URL = 'https://www.googleapis.com/youtube/v3/search';
+
+function getDataFromApi(searchTerm, callback) {
+  const query = {
+    q: `${searchTerm} in:title`,
+    part: 'snippet',
+    key: 'AIzaSyAgNHwiCAaJa1lR2K_gg1pcLguzpBdqb38'
+  }
+  $.getJSON(YOUTUBE_SEARCH_URL, query, callback);
+}
+
+function renderResult(result) {
+  return `
+  
+    <div> 
+      <a class="js-result-name" href="https://www.youtube.com/watch?v=${result.id.videoId}" target="_blank"><img src="${result.snippet.thumbnails.medium.url}" alt="${result.snippet.title}"></a>
+    </div>
+  `;
+}
+
+function displayYouTubeSearchData(data) {
+  const results = data.items.map((item, index) => renderResult(item));
+  $('.js-search-results').html(results);
+  $('.dataCount').text(results.length)
+}
+
+function watchSubmit() {
+  $('.js-search-form').submit(event => {
+    event.preventDefault();
+    const queryTarget = $(event.currentTarget).find('.js-query');
+    const query = queryTarget.val();
+    // clear out the input
+    queryTarget.val("");
+    getDataFromApi(query, displayYouTubeSearchData);
+  });
+}
+
+$(watchSubmit);
